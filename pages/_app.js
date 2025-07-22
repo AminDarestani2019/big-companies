f4b4b0a71358015325c383a0cd485d70ed06f750
Amin Darestani <darestani.amin@gmail.com>
2025-07-22 12:13:43 +0330

    change build to next

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,15 @@ import Layout from "@/components/layout/Layout";
 import "../styles/globals.css";
 import Head from "next/head";
 
-export default function MyApp({Component,pageProps}){
-  <>
-    <Head><meta name="viewport" content="width=device-width, initial-scale=1" /></Head>
-    return(
-      <Component {...pageProps} />
-        <Layout>
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
       </Layout>
-    );
-  </>
-  
+    </>
+  );
 }
